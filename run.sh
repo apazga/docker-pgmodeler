47014aced63cda9219f4134c75e84f80871167c1
Abel Paz <apazga@gmail.com>
2021-05-12 19:57:45 +0200

    Updated for pgModeler 0.9.4-alpha

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # Script to launch pgModeler
 #  - Use custom IP (192.168.1.100) for DISPLAY environment variable
 #  - Mounts ./data folder as /data
-docker run -ti -e DISPLAY=192.168.1.100:0.0 -v ./data:/data apazga/docker-pgmodeler:0.9.3
+docker run -ti -e DISPLAY=192.168.1.100:0.0 -v ./data:/data apazga/docker-pgmodeler:0.9.4-alpha
