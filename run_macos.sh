40a9b4cfec37fddb5a177c0e2ad0652e9ab6ae49
Antoine Merino <antoine.merino.dev@gmail.com>
2021-10-05 16:12:54 +0200

    Authorize local ip to authorized hosts everytime

--- run_macos.sh
+++ run_macos.sh
@@ -1,6 +1,10 @@
 # Script to launch pgModeler on macOS
 #  - Use host IP for DISPLAY environment variable
 #  - Mounts data folder in current directory to save pgModeler projects, config and plugins
+
+# Add local machine to authorized host by XQuartz
+xhost +
+
 HOST_IP=$(ifconfig en0 | grep "inet " | cut -d ' ' -f 2)
 echo HOST_IP: $HOST_IP
 docker run --rm --name="merinorus-pgmodeler" -e DISPLAY=$HOST_IP:0 -v /Users/myname/docker-pgmodeler/data/root:/root -v /Users/myname/docker-pgmodeler/data/usr/local/lib/docker-pgmodeler/plugins:/usr/local/lib/docker-pgmodeler/plugins merinorus/docker-pgmodeler:0.9.4-beta
