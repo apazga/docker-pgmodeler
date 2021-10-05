30393c4df55833699d285d15cfcdb01c60675a75
Antoine Merino <antoine.merino.dev@gmail.com>
2021-10-05 15:53:24 +0200

    change folders & remove container at exit

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-# Script to launch pgModeler
+# Script to launch pgModeler on Linux
 #  - Use custom IP (192.168.1.100) for DISPLAY environment variable
-#  - Mounts ./data folder as /data
-docker run -ti -e DISPLAY=192.168.1.100:0.0 -v ./data/root:/root -v ./data/usr/local/lib/pgmodeler/plugins:/usr/local/lib/pgmodeler/plugins merinorus/docker-pgmodeler:0.9.4-beta
+#  - Mounts data folder in current directory to save pgModeler projects, config and plugins
+docker run --rm -ti -e DISPLAY=192.168.1.100:0.0 -v /home/myname/pgmodeler/data/root:/root -v /home/myname/pgmodeler/data/usr/local/lib/pgmodeler/plugins:/usr/local/lib/pgmodeler/plugins merinorus/docker-pgmodeler:0.9.4-beta
