3573c16a34661e7c5635e2e9598a90d6111bea97
Antoine Merino <antoine.merino.dev@gmail.com>
2021-10-05 15:51:26 +0200

    documentation

--- run.ps1
+++ run.ps1
@@ -1,14 +1,10 @@
 <# 
     Script to launch pgModeler
-    - Use custom IP (192.168.1.100) for DISPLAY environment variable or try to find it automatically
-    - Mounts data folder (current directory) as /data
+    - Mounts data folder in current directory to save pgModeler projects, config and plugins
 #>
 
-# Use custom IP
-# Set-Variable -name DISPLAY -value 192.168.1.100:0.0
 
-# Try to get your LAN IP automatically
-Set-Variable -name IPADDR -value ((Get-NetIPAddress).IPAddress -like "192*").Trim()
+
 Set-Variable -name DISPLAY -value 'host.docker.internal:0.0'
 
 docker run --rm --name="merinorus-pgmodeler" -ti -e DISPLAY=$DISPLAY -v $PSScriptRoot/data/root:/root -v  $PSScriptRoot/data/usr/local/lib/pgmodeler/plugins:/usr/local/lib/pgmodeler/plugins merinorus/docker-pgmodeler:0.9.4-beta
