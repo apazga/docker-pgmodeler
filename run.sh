54f2ae008c495ffb0e56b9ee367b087c43a9d251
Antoine Merino <antoine.merino.dev@gmail.com>
2021-10-05 18:17:14 +0200

    example scripts & env files

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 # Script to launch pgModeler on Linux
 #  - Use custom IP (192.168.1.100) for DISPLAY environment variable
 #  - Mounts data folder in current directory to save pgModeler projects, config and plugins
-docker run --rm -ti -e DISPLAY=192.168.1.100:0.0 -v /home/myname/docker-pgmodeler/data/root:/root -v /home/myname/docker-pgmodeler/data/usr/local/lib/docker-pgmodeler/plugins:/usr/local/lib/docker-pgmodeler/plugins merinorus/docker-pgmodeler:0.9.4-beta
+
+# These settings are default one and should not be modified here, because a "git pull" would override your changes.
+# Instead, define your variables in a separate .env file.
+HOST_IP=192.168.1.100
+PGMODELER_IMAGE=merinorus/docker-pgmodeler:0.9.4-beta
+PROJECT_ROOT=/home/toto/docker-pgmodeler
+
+# Override environment variables with those from the .env file
+. .env.linux
+
+docker run --rm -ti -e DISPLAY=$HOST_IP:0.0 -v $PROJECT_ROOT/data/root:/root -v $PROJECT_ROOT/data/usr/local/lib/docker-pgmodeler/plugins:/usr/local/lib/docker-pgmodeler/plugins $PGMODELER_IMAGE
--- run_macos.sh
+++ run_macos.sh
@@ -2,9 +2,17 @@
 #  - Use host IP for DISPLAY environment variable
 #  - Mounts data folder in current directory to save pgModeler projects, config and plugins
 
+# These settings are default one and should not be modified here, because a "git pull" would override your changes.
+# Instead, define your variables in a separate .env file.
+HOST_IP=$(ifconfig en0 | grep "inet " | cut -d ' ' -f 2)
+PGMODELER_IMAGE=merinorus/docker-pgmodeler:0.9.4-beta
+PROJECT_ROOT=/Users/toto/docker-pgmodeler
+
+# Set environment variables from the .env file
+. .env
+
 # Add local machine to authorized host by XQuartz
 xhost +
 
-HOST_IP=$(ifconfig en0 | grep "inet " | cut -d ' ' -f 2)
 echo HOST_IP: $HOST_IP
-docker run --rm --name="merinorus-pgmodeler" -e DISPLAY=$HOST_IP:0 -v /Users/myname/docker-pgmodeler/data/root:/root -v /Users/myname/docker-pgmodeler/data/usr/local/lib/docker-pgmodeler/plugins:/usr/local/lib/docker-pgmodeler/plugins merinorus/docker-pgmodeler:0.9.4-beta
+docker run --rm --name="merinorus-pgmodeler" -e DISPLAY=$HOST_IP:0 -v $PROJECT_ROOT/data/root:/root -v $PROJECT_ROOT/data/usr/local/lib/docker-pgmodeler/plugins:/usr/local/lib/docker-pgmodeler/plugins $PGMODELER_IMAGE
